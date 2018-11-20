31be4ae57e465ae10ff541662b84b655abd8ecbe
Peter Øvergård Clausen <PeterOeClausen@gmail.com>
2018-11-20 15:30:19 +0100

    Refactoring LaugagevurDatasetInserter

--- ObjectCubeServerNetCore2/ObjectCubeServer/ConsoleAppForInteractingWithDatabase/LaugavegurDatasetInserter.cs
+++ ObjectCubeServerNetCore2/ObjectCubeServer/ConsoleAppForInteractingWithDatabase/LaugavegurDatasetInserter.cs
@@ -25,147 +25,154 @@ namespace ConsoleAppForInteractingWithDatabase
             
             string[] files = Directory.GetFiles(pathToDataset);
 
-            var insertPhotos = false;
+            var insertCubeObjects = false;
             var insertTags = false;
+            var insertHierarchies = false;
 
-            if (insertPhotos)
+            if (insertCubeObjects)
             {
-                //int numFilesToBeInserted = 2;
-                Console.WriteLine("Inserting photos as CubeObjects:");
-                int fileCount = 1;
-                using (var context = new ObjectContext())
-                {
-                    List<CubeObject> cubeObjects = new List<CubeObject>();
-                    foreach (string file in files)
-                    {
-                        string filename = Path.GetFileName(file);
-                        if (!filename.EndsWith(".csv"))
-                        {
-                            Console.WriteLine("Saving file: " + fileCount++ + " out of " + files.Length + " files. Filename: " + filename);
-                            //If Image is already in database:
-                            if (context.CubeObjects.Include(co => co.Photo).FirstOrDefault(co => co.Photo.FileName.Equals(filename)) != null)
-                            {
-                                //Don't add it again.
-                                Console.WriteLine("Image " + filename + " is already in the database");
-                            }
-                            //Else add it:
-                            else
-                            {
-                                //Loading and saving image:
-                                using (Image<Rgba32> image = SixLabors.ImageSharp.Image.Load(file))
-                                {
-                                    using (MemoryStream ms = new MemoryStream())
-                                    {
-                                        image.SaveAsJpeg(ms); //Copy to ms
-                                                              //Create and save cubeObject:
-                                        CubeObject cubeObject = DomainClassFactory.NewCubeObject(FileType.Photo, DomainClassFactory.NewPhoto(ms.ToArray(), Path.GetFileName(file)));
-                                        context.CubeObjects.Add(cubeObject);
-                                        context.SaveChanges();
-                                    }
-                                }
-                            }
-                        }
-                        /*
-                        numFilesToBeInserted--;
-                        if (numFilesToBeInserted == 0)
-                        {
-                            break;
-                        }
-                        */
-                    }
-                }
+                InsertCubeObjects(pathToDataset);
             }
+            else { Console.WriteLine("Skipping Photos"); }
 
             if (insertTags)
             {
-                //Inserting tagsets and tags:
-                Console.WriteLine("Inserting TagsSets and Tags:");
-                string[] linesInFile = File.ReadAllLines(pathToTagFile);
+                InsertTags(pathToTagFile, pathToErrorLogFile);
+            }
+            else { Console.WriteLine("Skipping Tags"); }
 
-                //int numIterations = 2;
-                int lineCount = 1;
-                foreach (string line in linesInFile)
+            if (insertHierarchies)
+            {
+                InsertHierarchies(pathToHierarchiesFile);
+            }
+            else { Console.WriteLine("Skipping Hierarchies"); }
+
+            Console.WriteLine("Done with InsertLaugavegurDataset");
+        }
+
+        private static void InsertCubeObjects(string pathToDataset)
+        {
+            string[] files = Directory.GetFiles(pathToDataset);
+
+            Console.WriteLine("Inserting photos as CubeObjects:");
+            using (var context = new ObjectContext())
+            {
+                List<CubeObject> cubeObjects = new List<CubeObject>();
+                int fileCount = 1;
+                foreach (string file in files)
                 {
-                    Console.WriteLine("Inserting line: " + lineCount++ + " out of " + linesInFile.Length);
-                    //File format: "FileName:TagSet:Tag:TagSet:Tag:(...)"
-                    string[] split = line.Split(":");
-                    string fileName = split[0];
-                    int numTagPairs = (split.Length - 2) / 2;
-                    for(int i = 0; i < numTagPairs; i++)
+                    string filename = Path.GetFileName(file);
+                    if (!filename.EndsWith(".csv"))
                     {
-                        string tagsetName = split[(i * 2) + 1];
-                        string tagName = split[(i * 2) + 2];
-                        using (var context = new ObjectContext())
+                        Console.WriteLine("Saving file: " + fileCount++ + " out of " + files.Length + " files. Filename: " + filename);
+                        //If Image is already in database (Assuming no two file has the same name):
+                        if (context.CubeObjects.Include(co => co.Photo).FirstOrDefault(co => co.Photo.FileName.Equals(filename)) != null)
                         {
-                            //Adding tagset to db:
-                            Tagset tagsetFromDb = context.Tagsets
-                                .Include(ts => ts.Tags)
-                                .FirstOrDefault(ts => ts.Name == tagsetName);
-                            //If tagset doesn't exist in db, add it:
-                            if (tagsetFromDb == null) 
+                            //Don't add it again.
+                            Console.WriteLine("Image " + filename + " is already in the database");
+                        }
+                        //Else add it:
+                        else
+                        {
+                            //Loading and saving image:
+                            using (Image<Rgba32> image = SixLabors.ImageSharp.Image.Load(file))
                             {
-                                tagsetFromDb = DomainClassFactory.NewTagSet(tagsetName);
-                                Tag tagWithSameNameAsTagset = DomainClassFactory.NewTag(tagsetName, tagsetFromDb);
-                                HelperMethods.AddTagToTagset(tagWithSameNameAsTagset, tagsetFromDb);
-
-                                context.Tagsets.Add(tagsetFromDb);
-                                context.Tags.Add(tagWithSameNameAsTagset);
-                                context.SaveChanges();
-                            }
-
-                            //Checking if tag exists, and creates it if it doesn't exist.
-                            Tag tagFromDb = context.Tags
-                                .Include(t => t.ObjectTagRelations)
-                                .Where(t => t.TagsetId == tagsetFromDb.Id)
-                                .FirstOrDefault(t => t.Name == tagName);
-                            //If tag doesn't exist in db, add it
-                            if (tagFromDb == null) 
-                            {
-                                tagFromDb = DomainClassFactory.NewTag(tagName, tagsetFromDb);
-                                //Tag does not have id yet, so we need to save it before adding it to tagsetFromDb's collection...
-
-                                context.Tags.Add(tagFromDb);
-                                context.SaveChanges();
-                            }
-
-                            //Add tag to tagset if tagset doesn't have it:
-                            if(!tagsetFromDb.Tags.Any(t => t.TagsetId == tagFromDb.Id)) //If tag does not exist in tagset, add it
-                            {
-                                HelperMethods.AddTagToTagset(tagFromDb, tagsetFromDb);
-                                context.SaveChanges();
-                            }
-
-                            //Adding tag to cube object with FileName:
-                            CubeObject cubeObjectFromDb = context.CubeObjects
-                                .Include(co => co.Photo)
-                                .Include(co => co.ObjectTagRelations)
-                                .FirstOrDefault(co => co.Photo.FileName.Equals(fileName));
-                            if(cubeObjectFromDb == null) //FileName does not exist in the database:
-                            {
-                                File.AppendAllText(pathToErrorLogFile, "Cube object with filename: " + fileName + " does not exist. Error occured while parsing line: " + lineCount + " in csv\n");
-                            }
-                            else
-                            {
-                                if(cubeObjectFromDb.ObjectTagRelations.FirstOrDefault(otr => otr.TagId == tagFromDb.Id) == null) //If Cubeobject does not already have tag asscociated with it, add it
+                                using (MemoryStream ms = new MemoryStream())
                                 {
-                                    HelperMethods.AddTagToObject(tagFromDb, cubeObjectFromDb);
+                                    image.SaveAsJpeg(ms); //Copy to ms
+                                                          //Create and save cubeObject:
+                                    CubeObject cubeObject = DomainClassFactory.NewCubeObject(FileType.Photo, DomainClassFactory.NewPhoto(ms.ToArray(), Path.GetFileName(file)));
+                                    context.CubeObjects.Add(cubeObject);
                                     context.SaveChanges();
                                 }
                             }
                         }
                     }
-
-                    /*
-                    numIterations--;
-                    if (numIterations == 0)
-                    {
-                        break;
-                    }
-                    */
                 }
             }
+        }
 
-            //Inserting hierarchies:
+        static void InsertTags(string pathToTagFile, string pathToErrorLogFile)
+        {
+            Console.WriteLine("Inserting TagsSets and Tags:");
+            string[] linesInFile = File.ReadAllLines(pathToTagFile);
+            int lineCount = 1;
+            foreach (string line in linesInFile)
+            {
+                Console.WriteLine("Inserting line: " + lineCount++ + " out of " + linesInFile.Length);
+                //File format: "FileName:TagSet:Tag:TagSet:Tag:(...)"
+                string[] split = line.Split(":");
+                string fileName = split[0];
+                int numTagPairs = (split.Length - 2) / 2;
+                for (int i = 0; i < numTagPairs; i++)
+                {
+                    string tagsetName = split[(i * 2) + 1];
+                    string tagName = split[(i * 2) + 2];
+                    using (var context = new ObjectContext())
+                    {
+                        //Adding tagset to db:
+                        Tagset tagsetFromDb = context.Tagsets
+                            .Include(ts => ts.Tags)
+                            .FirstOrDefault(ts => ts.Name == tagsetName);
+                        //If tagset doesn't exist in db, add it:
+                        if (tagsetFromDb == null)
+                        {
+                            tagsetFromDb = DomainClassFactory.NewTagSet(tagsetName);
+                            Tag tagWithSameNameAsTagset = DomainClassFactory.NewTag(tagsetName, tagsetFromDb);
+                            HelperMethods.AddTagToTagset(tagWithSameNameAsTagset, tagsetFromDb);
+
+                            context.Tagsets.Add(tagsetFromDb);
+                            context.Tags.Add(tagWithSameNameAsTagset);
+                            context.SaveChanges();
+                        }
+
+                        //Checking if tag exists, and creates it if it doesn't exist.
+                        Tag tagFromDb = context.Tags
+                            .Include(t => t.ObjectTagRelations)
+                            .Where(t => t.TagsetId == tagsetFromDb.Id)
+                            .FirstOrDefault(t => t.Name == tagName);
+                        //If tag doesn't exist in db, add it
+                        if (tagFromDb == null)
+                        {
+                            tagFromDb = DomainClassFactory.NewTag(tagName, tagsetFromDb);
+                            //Tag does not have id yet, so we need to save it before adding it to tagsetFromDb's collection...
+
+                            context.Tags.Add(tagFromDb);
+                            context.SaveChanges();
+                        }
+
+                        //Add tag to tagset if tagset doesn't have it:
+                        if (!tagsetFromDb.Tags.Any(t => t.TagsetId == tagFromDb.Id)) //If tag does not exist in tagset, add it
+                        {
+                            HelperMethods.AddTagToTagset(tagFromDb, tagsetFromDb);
+                            context.SaveChanges();
+                        }
+
+                        //Adding tag to cube object with FileName:
+                        CubeObject cubeObjectFromDb = context.CubeObjects
+                            .Include(co => co.Photo)
+                            .Include(co => co.ObjectTagRelations)
+                            .FirstOrDefault(co => co.Photo.FileName.Equals(fileName));
+
+                        if (cubeObjectFromDb == null)
+                        {
+                            File.AppendAllText(pathToErrorLogFile, "Cube object with filename: " + fileName + " does not exist. Error occured while parsing line: " + lineCount + " in csv\n");
+                        }
+                        else
+                        {
+                            if (cubeObjectFromDb.ObjectTagRelations.FirstOrDefault(otr => otr.TagId == tagFromDb.Id) == null) //If Cubeobject does not already have tag asscociated with it, add it
+                            {
+                                HelperMethods.AddTagToObject(tagFromDb, cubeObjectFromDb);
+                                context.SaveChanges();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void InsertHierarchies(string pathToHierarchiesFile)
+        {
             Console.WriteLine("Inserting Hierarchies");
             string[] allLines = File.ReadAllLines(pathToHierarchiesFile)
                 .Skip(1) //Skipping the first line cause it's documentation
@@ -173,26 +180,27 @@ namespace ConsoleAppForInteractingWithDatabase
             //File format: Parrent:IsRoot?:Child:Child:Child:(...)
             using (var context = new ObjectContext())
             {
-                foreach(string line in allLines)
+                foreach (string line in allLines)
                 {
                     string[] split = line.Split(":");
                     string parentHierarchyName = split[0];
                     bool isRoot = Boolean.Parse(split[1]);
 
-                    //Todo: refactor
-                    if (isRoot) //Tagset with same name must exist already.
+                    if (isRoot) //Assuming tagset with same name must exist already.
                     {
                         Tagset tagsetFromDb = context.Tagsets.Include(ts => ts.HierarchyRoots).FirstOrDefault(ts => ts.Name.Equals(parentHierarchyName));
                         Tag tagFromDb = context.Tags.FirstOrDefault(t => t.Name.Equals(parentHierarchyName));
                         Hierarchy newRootHierarchy = DomainClassFactory.NewHierarchy(tagFromDb, tagsetFromDb, null);
-                        
+
                         tagsetFromDb.HierarchyRoots.Add(newRootHierarchy);
                         context.Hierarchies.Add(newRootHierarchy);
+
                         for (int i = 2; i < split.Length; i++)
                         {
                             var childHierarchyName = split[i];
                             Tag tagFromDbWithHierarchyName = context.Tags.FirstOrDefault(t => t.Name.Equals(childHierarchyName));
-                            if(tagFromDbWithHierarchyName == null)
+                            //If tag does not exist:
+                            if (tagFromDbWithHierarchyName == null)
                             {
                                 tagFromDbWithHierarchyName = DomainClassFactory.NewTag(childHierarchyName, tagsetFromDb);
                                 context.Tags.Add(tagFromDbWithHierarchyName);
@@ -226,5 +234,6 @@ namespace ConsoleAppForInteractingWithDatabase
                 }
             }
         }
+
     }
 }
